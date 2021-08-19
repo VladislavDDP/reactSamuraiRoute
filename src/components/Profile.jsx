015d622b9cf643338ac3531cadc78ce1d8afd0e0
ilarionov <bombardire54@gmail.com>
2021-08-20 01:20:32 +0300

    Profile info and grids for profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,8 +5,15 @@ const Profile = () => {
         <div className={s.profile}>
             <h3>Profile description</h3>
             <img src="https://cdn-images-1.medium.com/max/2000/1*qXcjSfRj0C0ir2yMsYiRyw.jpeg" />
-            <h5>Vladyslav</h5>
-            <p>React developer</p>
+            <div className={s.profile_info}>
+                <h3>Vladyslav Denisovich</h3>
+                <ul>
+                    <li>Web developer</li>
+                    <li>student of 3d course</li>
+                    <li>React framework</li>
+                    <li>Django & Flask</li>
+                </ul>
+            </div>
         </div>
     )
 }
