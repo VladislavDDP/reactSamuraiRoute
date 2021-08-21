41b72787f8f0760d498ebecb51cff965e4553523
Vladislav <bombardire54@gmail.com>
2021-08-21 13:04:26 +0300

    Added props to tags and likes to posts on profile page

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,9 @@ import Post from './Post/Post'
 const MyPosts = () => {
     return (
         <div className={s.posts}>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post name='Vlad' text='Hello world' likes_count='4' />
+            <Post name='Vlad' text='Let`s speak about react?' likes_count='1' />
+            <Post name='Vlad' text='As for my plans for weekend...' likes_count='2' />
         </div>
     )
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,14 @@
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.post}>
             <img className={s.post_avatar} src="https://avatars.githubusercontent.com/u/79578649?v=4" alt="avatar" />
-            <p>My post</p>
+            <div>
+                <p>{ props.name }</p>   
+                <p>{ props.text }</p>  
+                <p>{ props.likes_count } like/-s </p>
+            </div>
         </div>
     )
 }
