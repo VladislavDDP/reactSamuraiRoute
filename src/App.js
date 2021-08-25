e262f611089d7866cb5f5f10891d7262fac77497
Vladislav <bombardire54@gmail.com>
2021-08-25 22:01:34 +0300

    Created ref for add post button

--- src/App.js
+++ src/App.js
@@ -7,29 +7,27 @@ import News from './components/Navbar/News/News'
 import Music from './components/Navbar/Music/Music'
 import Settings from './components/Navbar/Settings/Settings'
 import Footer from './components/Footer/Footer'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 const App = (props) => {
-  return (
-    <BrowserRouter>
-      <div className={s.app_wrapper}>
-        <Header />
-        <Navbar />
-        <div className={s.content}>
+return (
+    <div className={s.app_wrapper}>
+      <Header />
+      <Navbar />
+      <div className={s.content}>
 
-          <Route path='/profile' 
-                 render={() => <Profile 
-                                  posts={props.state.profilePage.posts} />} />
-          <Route path='/messages' 
-                 render={() => <Messages dialogs={props.state.dialogsPage} />} />
-          <Route path='/news' component={News} />
-          <Route path='/music' component={Music} />
-          <Route path='/settings' component={Settings} />
+        <Route path='/profile' 
+                render={() => <Profile 
+                                posts={props.state.profilePage.posts} />} />
+        <Route path='/messages' 
+                render={() => <Messages dialogs={props.state.dialogsPage} />} />
+        <Route path='/news' component={News} />
+        <Route path='/music' component={Music} />
+        <Route path='/settings' component={Settings} />
 
-        </div>
-        <Footer />
       </div>
-    </BrowserRouter>
+      <Footer />
+    </div>
   )
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,21 @@
+import React from 'react'
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = (props) => {
     let posts = props.posts.map(post => (<Post name={post.name} text={post.text} likes_count={post.likes_count}/>))
+
+    let newPostElement = React.createRef()
+    const addPost = () => {
+        const text = newPostElement.current.value
+        alert(text)
+    }
+    
     return (
         <div>
             <div className={s.add_post}>
-                <input className={s.input_post} type='text' placeholder='Type something...'/>
-                <button className={s.add_post_btn}>Add</button>
+                <input ref={newPostElement} className={s.input_post} type='text' placeholder='Type something...'/>
+                <button id='post_btn' onClick={addPost} className={s.add_post_btn}>Add</button>
             </div>
             <div className={s.posts}>
                 {posts}
