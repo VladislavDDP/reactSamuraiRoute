3e285d51a4ce3b005c5a26fb898a651a33c76e4c
Vladislav <bombardire54@gmail.com>
2021-08-26 15:44:14 +0300

    add message field and button

--- src/components/Messages/Dialogs/Dialogs.jsx
+++ src/components/Messages/Dialogs/Dialogs.jsx
@@ -1,12 +1,21 @@
 import s from './Dialogs.module.css'
 import Dialog from './Dialog/Dialog'
 import Message from './Message/Message'
+import React from 'react'
 
 const Dialogs = (props) => {
     let users = props.users.map(user => (
                     <Dialog id={user.id} name={user.name} />))
     let messages = props.messages.map(message => (
                     <Message text={message.text} sender={message.sender} />))
+
+    const sendMessageText = React.createRef()
+
+    const sendMessage = () => {
+        const messageText = sendMessageText.current.value
+        alert(messageText)
+    }
+    
     return (
         <div className={s.dialogs}>
             <div className={s.users}>
@@ -14,6 +23,11 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messsages}>
                 {messages}
+            </div>
+
+            <div className={s.send_message}>
+                <textarea className={s.messageTextarea} ref={sendMessageText} placeholder='Type message...' name="" rows="1"></textarea>
+                <button className={s.send_message_btn} onClick={sendMessage}>&gt;</button>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,17 +5,18 @@ import Post from './Post/Post'
 const MyPosts = (props) => {
     let posts = props.posts.map(post => (<Post name={post.name} text={post.text} likes_count={post.likes_count}/>))
 
-    let newPostElement = React.createRef()
+    const inputTextPost = React.createRef()
+
     const addPost = () => {
-        const text = newPostElement.current.value
+        const text = inputTextPost.current.value
         alert(text)
     }
     
     return (
         <div>
             <div className={s.add_post}>
-                <input ref={newPostElement} className={s.input_post} type='text' placeholder='Type something...'/>
-                <button id='post_btn' onClick={addPost} className={s.add_post_btn}>Add</button>
+                <input ref={inputTextPost} className={s.input_post} type='text' placeholder='Type something...'/>
+                <button onClick={addPost} className={s.add_post_btn}>Add</button>
             </div>
             <div className={s.posts}>
                 {posts}
