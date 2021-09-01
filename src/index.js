fcf18ea877904cd08f3ec81c2cadef6cb5f49d92
Vladislav <bombardire54@gmail.com>
2021-09-01 23:48:31 +0300

    Added own provider layer

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import StoreContext from './StoreContext'
+import { Provider } from './StoreContext'
 
 const renderTree = (state) => {
   debugger
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <StoreContext.Provider value={store}>
+        <Provider value={store}>
           <App />
-        </StoreContext.Provider> 
+        </Provider> 
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
