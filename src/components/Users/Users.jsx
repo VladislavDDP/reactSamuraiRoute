7ecbb370daba0b250728979386c2025973f9b734
Vladislav <bombardire54@gmail.com>
2021-09-07 11:06:14 +0300

    First class componet for user`s page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,36 @@
 import s from './Users.module.css'
 import * as axios from 'axios'
+import React from 'react'
 
-const Users = (props) => {
-    
-    const getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items))
+class Users extends React.Component {
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => this.props.setUsers(response.data.items))
         }
     }
-    
-    return (
-        <div>
-            <button onClick={getUsers}>Load users</button>
-            {
-                props.users.map(u => { 
-                    return <div key={u.id}>
-                        <img src={u.photos.small != null ? u.photos.small : 'https://pickaface.net/gallery/avatar/unr_random_160817_0304_2mvqp69.png'} className={s.profilePhoto} alt="" />
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                        <div>{"u.location.city"}</div>
-                        <div>{"u.location.country"}</div>
 
-                        {u.followed
-                            ? <button onClick={() => {props.unfollowUser(u.id)} }>Unfollow</button> 
-                            : <button onClick={() => {props.followUser(u.id)} }>Follow</button>}
-                    </div>
-                })
-            }
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <button onClick={this.getUsers}>Load users</button>
+                {
+                    this.props.users.map(u => { 
+                        return <div key={u.id}>
+                            <img src={u.photos.small != null ? u.photos.small : 'https://pickaface.net/gallery/avatar/unr_random_160817_0304_2mvqp69.png'} className={s.profilePhoto} alt="" />
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                            <div>{"u.location.city"}</div>
+                            <div>{"u.location.country"}</div>
+    
+                            {u.followed
+                                ? <button onClick={() => {this.props.unfollowUser(u.id)} }>Unfollow</button> 
+                                : <button onClick={() => {this.props.followUser(u.id)} }>Follow</button>}
+                        </div>
+                    })
+                }
+            </div>
+        )
+    }
 }
 
 export default Users
