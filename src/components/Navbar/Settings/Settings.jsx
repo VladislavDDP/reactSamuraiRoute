ea6c4acb0840eceb9a8d3c5a27e32ee35ec31d96
Vladislav <bombardire54@gmail.com>
2021-09-20 23:54:36 +0300

    Updated settings content

--- src/components/Navbar/Settings/Settings.jsx
+++ src/components/Navbar/Settings/Settings.jsx
@@ -1,15 +1,25 @@
 import s from './Settings.module.css'
 
 const Settings = (props) => {
+
+    const changeLight = () => {
+        console.log('light mode')
+    }
+
+    
+    const changeDark = () => {
+        console.log('dark mode')
+    }
+
     return (
         <div className={s.settings}>
             <div>Your Settings</div>
             <br /> <br />
             <div>Choose theme: </div>
             <form action="">
-                Light<input type="radio" name='theme' onClick={} />
                 <br />
-                Dark<input type="radio" name='theme' onClick={} />
+                Light<input type="radio" name='theme' onClick={changeLight} />
+                Dark<input type="radio" name='theme' onClick={changeDark} checked />
             </form>
         </div>
     )
