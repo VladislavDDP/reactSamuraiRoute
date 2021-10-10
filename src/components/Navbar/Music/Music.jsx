8c46ec8bdd610d83dde085ea25126e768583dda8
Vladislav <bombardire54@gmail.com>
2021-10-10 23:28:07 +0300

    Design some useless components like music and news

--- src/components/Navbar/Music/Music.jsx
+++ src/components/Navbar/Music/Music.jsx
@@ -3,12 +3,11 @@ import s from './Music.module.css'
 const Music = (props) => {
     return (
         <div className={s.music}>
-            <div>Your Music</div>
-
-            <div>Track 1</div>
-            <div>Track 2</div>
-            <div>Track 3</div>
-            <div>Track 4</div>
+            <div>Your Music:</div>
+            <audio className={s.track} controls src="https://www.computerhope.com/jargon/m/example.mp3">1</audio>
+            <audio className={s.track} controls src="https://www.computerhope.com/jargon/m/example.mp3">2</audio>
+            <audio className={s.track} controls src="https://www.computerhope.com/jargon/m/example.mp3">3</audio>
+            <audio className={s.track} controls src="https://www.computerhope.com/jargon/m/example.mp3">4</audio>
         </div>
     )
 }
